5200afdaf881495929bda59a4f0294903db41a33
John Xu <haoxuanxu@uchicago.edu>
2023-02-21 10:44:50 -0500

    debug

--- PairTrading/trading/manager.py
+++ PairTrading/trading/manager.py
@@ -170,8 +170,8 @@ class TradingManager(Base, metaclass=Singleton):
                          
     def _getCloseablePairs(self, openedPositions:dict[str, Position]) -> list[tuple]:
         
-        print(abs((datetime.now().replace(tzinfo=self.clock.timestamp.tzinfo) - self.clock.timestamp).total_seconds()))
-        updateLogTime:bool = abs((datetime.now().replace(tzinfo=self.clock.timestamp.tzinfo) - self.clock.timestamp).total_seconds()) >= 60
+        print((datetime.now().replace(tzinfo=pytz.UTC) - self.clock.timestamp.replace(tzinfo=pytz.UTC)).seconds)
+        updateLogTime:bool = (datetime.now().replace(tzinfo=pytz.UTC) - self.clock.timestamp.replace(tzinfo=pytz.UTC)).seconds >= 60
         res:list[tuple] = []        
         openedPairs:dict[tuple, float] = self.tradingRecord     
         openedPairsPositions:dict[tuple, list] = self.pairInfoRetriever.getCurrentlyOpenedPairs(
