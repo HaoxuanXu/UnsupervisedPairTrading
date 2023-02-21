9392120fdd79275c160d59f8985e8bbdc635e7b2
John Xu <haoxuanxu@uchicago.edu>
2023-02-21 12:21:08 -0500

    debug

--- lib/patterns/retry.py
+++ lib/patterns/retry.py
@@ -5,7 +5,7 @@ import logging
 def retry(max_retries=3, retry_delay=1, incremental_backoff=2, logger=None):
     def decorator(func):
         def wrapper(*args, **kwargs):
-            retry_delay = retry_delay
+            delay = retry_delay
             retries = 0
             while retries < max_retries:
                 try:
@@ -16,8 +16,8 @@ def retry(max_retries=3, retry_delay=1, incremental_backoff=2, logger=None):
                         raise(e)
                     if logger:
                         logger.warning(f"Retryable error caught: {e}. Retrying...")
-                    time.sleep(retry_delay)
-                    retry_delay *= incremental_backoff
+                    time.sleep(delay)
+                    delay *= incremental_backoff
         return wrapper
     return decorator
 
