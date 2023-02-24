9784f5545b868d3e705d3c1dbe62f1fec5e14f75
John Xu <haoxuanxu@uchicago.edu>
2023-02-24 10:24:35 -0500

    only work on etf

--- MACDTrading/etfs.py
+++ MACDTrading/etfs.py
@@ -27,12 +27,15 @@ class ETFs(Base, metaclass=Singleton):
     def getAllCandidates(self) -> dict[str, list]:
         
         res:dict[str, list] = {}
-        res["NASDAQ"] = [equity.symbol for equity in self.tradingClient.allTradableStocks(exchanges=[AssetExchange.NASDAQ])
-                            if self.dataClient.getMarketCap(equity.symbol) > 1_000_000]
+        # res["NASDAQ"] = [equity.symbol for equity in self.tradingClient.allTradableStocks(exchanges=[AssetExchange.NASDAQ])
+        #                     if self.dataClient.getMarketCap(equity.symbol) > 1_000_000]
         res["ETF"] = [equity.symbol for equity in self.tradingClient.allTradableStocks(exchanges=[AssetExchange.ARCA]) 
                       if self.dataClient.getMarketCap(equity.symbol) > 1_000_000]
-        res["NYSE_AMEX"] = [equity.symbol for equity in self.tradingClient.allTradableStocks(exchanges=[AssetExchange.NYSE, AssetExchange.AMEX])
-                            if self.dataClient.getMarketCap(equity.symbol) > 1_000_000]
+        
+        res["NASDAQ"] = []
+        res["NYSE_AMEX"] = []
+        # res["NYSE_AMEX"] = [equity.symbol for equity in self.tradingClient.allTradableStocks(exchanges=[AssetExchange.NYSE, AssetExchange.AMEX])
+        #                     if self.dataClient.getMarketCap(equity.symbol) > 1_000_000]
                 
         return res 
         
