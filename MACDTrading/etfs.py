3b254fb9d1827d76cc3425601b59900050e4e8bf
John Xu <haoxuanxu@uchicago.edu>
2023-02-27 17:37:28 -0500

    debug

--- MACDTrading/etfs.py
+++ MACDTrading/etfs.py
@@ -22,6 +22,7 @@ class ETFs(Base, metaclass=Singleton):
         )
         
     def getAllCandidates(self) -> dict[str, list]:      
-        return [equity.symbol for equity in self.tradingClient.allTradableStocks(exchanges=[AssetExchange.ARCA, AssetExchange.BATS])]
+        return [equity.symbol for equity in self.tradingClient.allTradableStocks(exchanges=[AssetExchange.ARCA, AssetExchange.BATS])
+                if self.dataClient.getMarketCap(equity.symbol) > 1_000_000]
 
         
